82242dafc7cd2793540114e76c4969a76cb10c76
Srinivas Kodali <srinivas.kodali@infor.com>
2020-01-29 16:33:41 -0600

    Reverse String in place recursion

---
+++ src/com/company/ReverseString.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        reverse(0, s.length-1, s);
+        System.out.println(s);
+    }
+
+    private void reverse(int start, int end, char[] s) {
+        if(start >= end) {
+            return;
+        }
+        char t = s[start];
+        s[start] = s[end];
+        s[end] = t;
+        reverse(++start, --end, s);
+    }
+
+    public static void main(String[] args) {
+        ReverseString reverseString = new ReverseString();
+        reverseString.reverseString(new char[] {'h', 'e', 'l', 'l', 'o'});
+    }
+}
