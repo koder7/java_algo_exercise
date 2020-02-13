880ca92391762d24a1d604e6d64abd4788bdf8e8
Srinivas Kodali <srinivas.kodali@infor.com>
2020-02-13 16:10:32 -0600

    Valid BST

---
+++ src/problems/leetcode/divideandconquer/TreeNode.java
@@ -0,0 +1,9 @@
+package problems.leetcode.divideandconquer;
+
+// * Definition for a binary tree node.
+ public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
---
+++ src/problems/leetcode/divideandconquer/ValidBST.java
@@ -0,0 +1,40 @@
+package problems.leetcode.divideandconquer;
+
+import java.util.Stack;
+
+public class ValidBST {
+    public boolean helper(TreeNode node, Integer lower, Integer upper) {
+        if (node == null) return true;
+
+        int val = node.val;
+        if (lower != null && val <= lower) return false;
+        if (upper != null && val >= upper) return false;
+
+        if (!helper(node.right, val, upper)) return false;
+        if (!helper(node.left, lower, val)) return false;
+        return true;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    // check if it obeys in-order traversal
+    public boolean isValidBST_iterative(TreeNode root) {
+        double inorder = - Double.MAX_VALUE;
+
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= inorder) return false;
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+}
