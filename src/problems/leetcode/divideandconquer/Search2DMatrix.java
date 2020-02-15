70f7b3e36b28a574d50ef62c445f54b1fe490733
Srinivas Kodali <srinivas.kodali@infor.com>
2020-02-15 13:17:00 -0600

    Search 2D array

---
+++ src/problems/leetcode/divideandconquer/Search2DMatrix.java
@@ -0,0 +1,21 @@
+package problems.leetcode.divideandconquer;
+
+public class Search2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length-1;
+        int column = 0;
+
+        while( row >= 0 && column < matrix[0].length) {
+            if(matrix[row][column] > target) {
+                row--;
+            } else if( matrix[row][column] < target) {
+                column++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
