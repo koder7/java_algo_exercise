01d53ac9c2816918f6f162d0ce47e4b4b503d76b
Srinivas Kodali <srinivas.kodali@infor.com>
2020-02-16 16:57:54 -0600

    shortest distance to character

--- src/problems/leetcode/random/ShortestDistanceToCharacter.java
+++ src/problems/leetcode/random/ShortestDistanceToCharacter.java
@@ -1,5 +1,36 @@
 package problems.leetcode.random;
 
-public class ShortestDistanceToCharacter {
+import problems.leetcode.recursion.MatchPow;
 
+import java.util.Arrays;
+
+public class ShortestDistanceToCharacter {
+    public int[] shortestToChar(String S, char C) {
+        if(S.isEmpty() || S == null) {
+            return null;
+        }
+        int n = S.length();
+        int[] output = new int[n];
+        int prev = Integer.MIN_VALUE/2;
+        for (int i = 0; i < n; i++) {
+            if(S.charAt(i) == C) {
+                prev = i;
+            }
+            output[i] = i - prev;
+        }
+        prev = Integer.MAX_VALUE;
+        for (int i = n-1; i >= 0 ; i--) {
+            if(S.charAt(i) == C) {
+                prev = i;
+            }
+            output[i] = Math.min(output[i], prev-i);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        ShortestDistanceToCharacter p = new ShortestDistanceToCharacter();
+
+        System.out.println(Arrays.toString(p.shortestToChar("loveleetcode", 'e')));
+    }
 }
