33221d3128a3aaf536f824f64e4257e06d9b1977
Srinivas Kodali <srinivas.kodali@infor.com>
2020-02-16 17:33:18 -0600

    closest three sum

---
+++ src/problems/leetcode/random/ClosestThreeSum.java
@@ -0,0 +1,31 @@
+package problems.leetcode.random;
+
+import java.util.Arrays;
+
+public class ClosestThreeSum {
+    int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Illegal input");
+        }
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+                if (sum == target) {
+                    return sum;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return closest;
+    }
+}
